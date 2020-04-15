0bf1b4c9593a7bd78e0713328c09a04caf8d0d0d
AugustoMarcelo <mrclgst10@gmail.com>
2020-04-14 22:20:31 -0300

    Implementadas as rotas; resta passar no teste de importação

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,44 @@
+import parseCSV from 'csv-parse';
+import path from 'path';
+import fs from 'fs';
+
+import uploadConfig from '../config/upload';
+
 import Transaction from '../models/Transaction';
+import CreateTransactionService from './CreateTransactionService';
+
+interface Request {
+  filename: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute({ filename }: Request): Promise<Transaction[]> {
+    const lines = parseCSV({ from_line: 2 });
+
+    const filePath = path.join(uploadConfig.directory, filename);
+    const readStream = fs.createReadStream(filePath);
+
+    const parsedCSV = readStream.pipe(lines);
+
+    const transactions: Transaction[] = [];
+    const createTransaction = new CreateTransactionService();
+
+    parsedCSV.on('data', async line => {
+      const [title, type, value, category] = line;
+
+      const transaction = await createTransaction.execute({
+        title,
+        type,
+        value,
+        category,
+      });
+
+      transactions.push(transaction);
+    });
+
+    await new Promise(resolve => parsedCSV.on('end', resolve));
+
+    return transactions;
   }
 }
 
